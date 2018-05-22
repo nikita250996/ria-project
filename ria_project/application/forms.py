778f2be6c432f997a9de06c00b7373e940161bad
python-eats-ruby <badarchysayan@gmail.com>
2018-05-22 10:11:53 +0700

    fix forms and views

--- ria_project/application/forms.py
+++ ria_project/application/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from application.models import IntellectualProperty
+from application.models import IntellectualProperty, Payment
 
 
 class RequestIntellectualPropertyForm(forms.ModelForm):
@@ -56,4 +56,23 @@ class ContractIntellectualPropertyForm(forms.ModelForm):
 
     class Meta:
         model = IntellectualProperty
+        fields = '__all__'
+
+
+class PaymentForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(PaymentForm, self).__init__(*args, **kwargs)
+
+        for _, field in self.fields.items():
+            field.widget.attrs = {
+                'class': 'form-control',
+                'placeholder': field.help_text,
+            }
+
+        self.fields['payment_date'].widget.attrs['type']='date'
+        self.fields['posted_date'].widget.attrs['type'] = 'date'
+
+    class Meta:
+        model = Payment
         fields = '__all__'
